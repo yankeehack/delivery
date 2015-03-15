d3e6ffb9d1f9b65ada5b4fb836cb86854a9e0a78
yankeehack <zhuyanqing1989@gmail.com>
2015-03-14 22:40:45 -0700

    starting version of rollingman webpage

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,12 +2,15 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.index;
+import views.html.basic;
+import views.html.carousel;
 
 public class Application extends Controller {
-
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+  public static Result basic() {
+    return ok(basic.render("Your new application is ready."));
+  }
+  public static Result withDropDown(){
+    return ok(carousel.render("rolling man is ready"));
+  }
 
 }
